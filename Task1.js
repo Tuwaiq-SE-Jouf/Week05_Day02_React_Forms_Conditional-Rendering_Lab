cbca55141da07a6669c6b1597154c3d4a54d5448
ManalAlkuraya <ManalAlkuraya1@gmail.com>
2021-11-08 21:10:06 +0300

    Manal

--- Task1.js
+++ Task1.js
@@ -1,4 +1,72 @@
-function App() {
-  return <div>Code goes here</div>;
+
+import React, { Component } from "react"
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = {
+            isLoggedIn: false
+        };
+    }
+
+    handleLoginClick() {
+        this.setState({ isLoggedIn: true });
+    }
+
+    handleLogoutClick() {
+        this.setState({ isLoggedIn: false });
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        let button;
+
+        if (isLoggedIn) {
+            button = <LogoutButton onClick={this.handleLogoutClick} />;
+        } else {
+            button = <LoginButton onClick={this.handleLoginClick} />;
+        }
+        return (
+            <div>
+                <Greeting isLoggedIn={isLoggedIn} />
+                {button}
+            </div>
+        );
+    }
 }
-export default App;
+
+function UserGreeting(props) {
+    return <h1>ogged in</h1>;
+}
+
+function GuestGreeting(props) {
+    return <h1> ogged out</h1>;
+}
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+        return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+}
+
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    );
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    );
+}
+export default App
