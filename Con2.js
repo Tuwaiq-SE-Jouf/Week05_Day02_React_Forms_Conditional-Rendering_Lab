6e49f6cefd82a85230d46e1b7a03f374df69cae3
halah03 <mud2200@gmail.com>
2021-11-09 02:10:37 +0300

    Hm&lap

---
+++ Con2.js
@@ -0,0 +1,14 @@
+import React from "react";
+export function Loggedin() {
+    return (
+      <div>
+       
+       <button onClick={this.handleClick}>logout</button>
+               
+      </div>
+    );
+  }
+  
+
+export default Loggedin;
+
