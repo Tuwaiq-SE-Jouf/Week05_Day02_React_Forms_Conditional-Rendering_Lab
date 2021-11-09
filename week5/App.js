0dc3a711618f7a535544463b31fbad68e73cee32
gar505gar <grm_505@hotmail.com>
2021-11-09 23:39:47 +0300

    w.h

---
+++ week5/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import FormCo from "./event/FormContainer";
+import FormCom from "./event/FormComponent";
+import FormContainer from "./FormContainer";
+function App(){
+  return <FormContainer/>
+   
+  }
+
+
+    export default App;
+
