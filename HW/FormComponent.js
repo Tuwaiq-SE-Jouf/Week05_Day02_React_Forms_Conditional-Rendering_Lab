55c5680be546ef07d263e62b594fd4015c5ee823
sultanhalruwaili <lllx800@gmail.com>
2021-11-09 12:59:57 +0300

    Update HW

---
+++ HW/FormComponent.js
@@ -0,0 +1,113 @@
+import React  from "react";
+
+ function FormComponent (props){
+  return (<div>
+  <form>
+    <input
+      type="text"
+      value={props.data.firstName}
+      name="firstName"
+      placeholder="First Name"
+      onChange={props.handleChange}
+      />
+    <br />
+    <input
+     type="text"
+     value={props.data.lastName}
+     name="lastName"
+     placeholder="Last Name"
+     onChange={props.handleChange}
+     />
+    <br />
+    <input
+    placeholder="Age"
+    type="text"
+    value={props.data.age}
+    name="age"
+    onChange={props.handleChange}/>
+    <br />
+    <label>
+    <input
+      type="radio"
+      name="gender"
+      value="male"
+      checked={props.data.gender === "male"}
+      onChange={props.handleChange}
+    />{" "}
+    Male
+  </label>
+  <br />
+  <label>
+    <input
+      type="radio"
+      name="gender"
+      value="female"
+      checked={props.data.gender === "female"}
+      onChange={props.handleChange}
+    />{" "}
+    Female
+  </label>
+
+    <br />
+    <label>Your Location:</label>
+  <select
+    value={props.data.gLocation}
+    onChange={props.handleChange}
+    name="gLocation"
+  >
+    <option value="select location">Please select your location</option>
+    <option value="Dummah Al Jandal">Dummah Al Jandal</option>
+    <option value="Riyadh">Riyadh</option>
+    <option value="Jeddah">Jeddah</option>
+    <option value="Sakkakah">Sakkakah</option>
+  </select>
+    
+    <br />
+    <label>
+    <input
+      type="checkbox"
+      name="kosher"
+      checked={props.data.kosher}
+      onChange={props.handleChange}
+    />{" "}
+      Kosher
+  </label>
+  <label>
+    <input
+      type="checkbox"
+      name="Halal"
+      checked={props.data.Halal}
+      onChange={props.handleChange}
+    />{" "}
+      Halal
+  </label>
+  <label>
+    <input
+      type="checkbox"
+      name="vegan"
+      checked={props.data.vegan}
+      onChange={props.handleChange}
+    />{" "}
+      Vegan
+  </label>
+  
+    <br />
+    <button>Submit</button>
+  </form>
+  <hr />
+  <h2>Entered information:</h2>
+  <p>Your name: {props.data.firstName} {props.data.lastName}</p>
+  <p>Your age: {props.data.age}</p>
+  <p>Your gender: {props.data.gender}</p>
+  <p>Your destination: {props.data.gLocation}</p>
+  <p>
+    Your dietary restrictions:
+  </p>
+  <p id="Kosher">
+  </p>
+  <p id="Halal"></p>
+  <p id="Vegan"></p>
+  </div>
+  )
+ }
+ export default FormComponent
